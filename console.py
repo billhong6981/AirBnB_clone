46782bf1b30563e5be541806d59f25cc27a8601a
billhong6981 <bill.burton6981@gmail.com>
2019-06-29 22:23:16 +0000

    finished create command

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import cmd
 import sys
+from models.base_model import BaseModel
 """
 """
 
@@ -10,11 +11,26 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb) '
 
+    def cmdloop(self):
+        return cmd.Cmd.cmdloop(self)
+
+    def parseline(self, line):
+        ret = cmd.Cmd.parseline(self, line)
+        return ret
+
+    def onecmd(self, s):
+        return cmd.Cmd.onecmd(self, s)
+
+    def emptyline(self):
+        pass
+
+    def default(self, line):
+        return cmd.Cmd.default(self, line)
+
     def do_quit(self, arg):
         """
         """
-        sys.exit()
-        return True
+        sys.exit(0)
 
     def help_quit(self):
         print('Quit command to exit the program')
@@ -26,8 +42,44 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
-    def emptyline(self):
-            pass
+    def do_create(self, line):
+        """
+        """
+        if line is None or line == "":
+            print("** class name missing **")
+            return False
+        arg = line.split()
+        try:
+            obj = (eval(arg[0]))()
+            print(obj.id)
+        except NameError:
+            print("** class doesn't exist **")
+            return False
+        #print("arg[0]: {} arg[1]: {} arg[2]: {}".format(arg[0], arg[1], arg[2]))
+
+
+    def do_show(self, line):
+        """
+        """
+        pass
+
+    def do_destroy(self, arg):
+        """
+        """
+        pass
+
+    def do_all(self, arg):
+        """
+        """
+        pass
+
+    def do_update(self, arg):
+        """
+        """
+        pass
+
+    def do_greet(self, line):
+        print("Hello, ", line)
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
