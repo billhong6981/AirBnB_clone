cb9afcfa7b9ce63c0317e6e67591ad416455e745
billhong6981 <bill.burton6981@gmail.com>
2019-06-29 22:46:25 +0000

    add documentation

--- console.py
+++ console.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""my console dashboard module"""
+
 import cmd
 import sys
 from models.base_model import BaseModel
-"""
-"""
 
 
 class HBNBCommand(cmd.Cmd):
     """
-    """
+    my console dashboard class"""
+
     prompt = '(hbnb) '
 
     def cmdloop(self):
@@ -29,21 +30,27 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """
+        my quit function
         """
         sys.exit(0)
 
     def help_quit(self):
+        """
+        my help function
+        """
         print('Quit command to exit the program')
         return True
 
     def do_EOF(self, arg):
         """
+        my alternative quit function
         """
         print()
         return True
 
     def do_create(self, line):
         """
+        create new instance function
         """
         if line is None or line == "":
             print("** class name missing **")
@@ -58,24 +65,27 @@ class HBNBCommand(cmd.Cmd):
             return False
         #print("arg[0]: {} arg[1]: {} arg[2]: {}".format(arg[0], arg[1], arg[2]))
 
-
     def do_show(self, line):
         """
+        display the information about instance of class
         """
         pass
 
     def do_destroy(self, arg):
         """
+        deletes the instance of class
         """
         pass
 
     def do_all(self, arg):
         """
+        display all instances of the same class
         """
         pass
 
     def do_update(self, arg):
         """
+        updates the new info to the existing instance
         """
         pass
 
