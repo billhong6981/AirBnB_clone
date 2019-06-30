2a6a4856cd0b97879cd207a4dc6709108ac99641
billhong6981 <bill.burton6981@gmail.com>
2019-06-30 00:12:23 +0000

    finished the show command

--- console.py
+++ console.py
@@ -4,6 +4,8 @@
 import cmd
 import sys
 from models.base_model import BaseModel
+from models import storage
+#from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -63,13 +65,34 @@ class HBNBCommand(cmd.Cmd):
         except NameError:
             print("** class doesn't exist **")
             return False
-        #print("arg[0]: {} arg[1]: {} arg[2]: {}".format(arg[0], arg[1], arg[2]))
 
     def do_show(self, line):
         """
         display the information about instance of class
         """
-        pass
+        if line is None or line == "":
+            print("** class name missing **")
+            return False
+        arg = line.split()
+        try:
+            obj = (eval(arg[0]))()
+        except NameError:
+            print("** class doesn't exist **")
+            return False
+
+        if arg[1] is None or arg[1] == "":
+            print("** instance id missing **")
+            return False
+
+        all_objs = storage.all()
+        search = (arg[0] + "." + arg[1])
+        try:
+            ins = all_objs[search]
+            print(ins)
+            return True
+        except NameError:
+            print("** no instance found **")
+            return False
 
     def do_destroy(self, arg):
         """
