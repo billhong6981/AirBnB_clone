0356a83173b5f605c457d56d528b627452ef80ac
billhong6981 <bill.burton6981@gmail.com>
2019-06-30 20:58:50 +0000

    fix the bug of everytime running create a new instance

--- console.py
+++ console.py
@@ -74,13 +74,11 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return False
         arg = line.split()
-        try:
-            obj = (eval(arg[0]))()
-        except NameError:
+        if arg[0] != "BaseModel":
             print("** class doesn't exist **")
             return False
 
-        if arg[1] is None or arg[1] == "":
+        if len(arg) < 2:
             print("** instance id missing **")
             return False
 
@@ -89,7 +87,6 @@ class HBNBCommand(cmd.Cmd):
         try:
             ins = all_objs[search]
             print(ins)
-            return True
         except KeyError:
             print("** no instance found **")
             return False
@@ -102,13 +99,11 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return False
         arg = line.split()
-        try:
-            obj = (eval(arg[0]))()
-        except NameError:
+        if arg[0] != "BaseModel":
             print("** class doesn't exist **")
             return False
 
-        if arg[1] is None or arg[1] == "":
+        if len(arg) < 2:
             print("** instance id missing **")
             return False
 
@@ -116,25 +111,65 @@ class HBNBCommand(cmd.Cmd):
         search = (arg[0] + "." + arg[1])
         try:
             del all_objs[search]
-            obj.save()
+            storage.save() #obj.save()
         except KeyError:
             print("** no instance found **")
             return False
 
-    def do_all(self, arg):
+    def do_all(self, line):
         """
         display all instances of the same class
         """
-        pass
+        string = ""
+        list_a = []
+        if line is not None and line != "":
+            arg = line.split()
+            if arg[0] != "BaseModel":
+                print("** class doesn't exist **")
+                return False
+        all_objs = storage.all()
+        for k, v in all_objs.items():
+            list_a.append(str(v))
+            #string += str(v)
+        #print("[\"{}\"]".format(string))
+        print(list_a)
 
-    def do_update(self, arg):
+    def do_update(self, line):
         """
         updates the new info to the existing instance
         """
-        pass
+        if line is None or line == "":
+            print("** class name missing **")
+            return False
+        arg = line.split()
+        if arg[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return False
 
-    def do_greet(self, line):
-        print("Hello, ", line)
+        if len(arg) < 2:
+            print("** instance id missing **")
+            return False
+
+        all_objs = storage.all()
+        #all_objs = storage.objects
+        search = (arg[0] + "." + arg[1])
+        try:
+            ins = all_objs[search]
+        except KeyError:
+            print("** no instance found **")
+            return False
+
+        if len(arg) < 3:
+            print("** attribute name missing **")
+            return False
+        if len(arg) < 4:
+            print("** value missing **")
+            return False
+
+        if search in all_objs:
+            setattr(all_objs[search], arg[2], arg[3][1:-1])
+            #obj.save()
+            storage.save()
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
