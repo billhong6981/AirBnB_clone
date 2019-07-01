c01b29b98f2995bcdcbc3ba313556f102005e443
VieetBubbles <537@holbertonschool.com>
2019-07-01 20:31:25 +0000

    Initial Commit.

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+"""
+from models.base_model import BaseModel
+
+
+class City:
+    """
+    """
+    state_id = ""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+"""
+from models.base_model import BaseModel
+
+
+class Review:
+    """
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
